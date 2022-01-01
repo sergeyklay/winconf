bceff60544678a82c8d52492a150b7eecd64f9f9
Serghei Iakovlev <egrep@protonmail.ch>
2022-01-01 18:31:07 +0200

    Add oh-my-posh config

--- sync.ps1
+++ sync.ps1
@@ -1,1 +1,2 @@
-Copy-Item -Path $PROFILE -Destination .
+Copy-Item -Path .\Microsoft.PowerShell_profile.ps1 -Destination $PROFILE.CurrentUserCurrentHost
+Copy-Item -Path .\ohmyposhv3-v2.json -Destination $env:USERPROFILE\ohmyposhv3-v2.json
