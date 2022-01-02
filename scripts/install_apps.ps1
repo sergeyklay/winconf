c5377d716ec52c7e2c3c0c4db6259ee095319e2b
Serghei Iakovlev <egrep@protonmail.ch>
2022-01-02 18:43:24 +0200

    Install DB Browser For SQLite

--- scripts/install_apps.ps1
+++ scripts/install_apps.ps1
@@ -15,20 +15,35 @@ foreach ($app in $common_apps) {
     winget install --exact --id $app
 }
 
+Write-Host "Installing terminal support..."
+Write-Host "------------------------------------" -ForegroundColor Green
+$terminal_apps = @(
+    "Microsoft.WindowsTerminal",
+    "JanDeDobbeleer.OhMyPosh")
+foreach ($app in $terminal_apps) {
+    winget install --exact --id $app
+}
+
+Write-Host "Installing dotnet support..."
+Write-Host "------------------------------------" -ForegroundColor Green
+$dotnet_apps = @(
+    "Microsoft.dotnet",
+    "Microsoft.dotNetFramework",
+    "Microsoft.dotnetRuntime.6-x64")
+foreach ($app in $dotnet_apps) {
+    winget install --exact --id $app
+}
+
 Write-Host "Installing dev essentials..."
 Write-Host "------------------------------------" -ForegroundColor Green
 
 $dev_essentials = @(
     "Docker.DockerDesktopEdge",
-    "Microsoft.WindowsTerminal",
-    "JanDeDobbeleer.OhMyPosh",
     "Microsoft.VisualStudioCode",
     "vim.vim",
     "Git.Git",
     "GnuPG.Gpg4win",
-    "Microsoft.dotnet",
-    "Microsoft.dotNetFramework",
-    "Microsoft.dotnetRuntime.6-x64")
+    "DBBrowserForSQLite.DBBrowserForSQLite")
 foreach ($app in $dev_essentials) {
     winget install --exact --id $app
 }
